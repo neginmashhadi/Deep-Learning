5272c8f565bc19baadfab2d937625333f7e91418
Negin <negin.mashhadi@gmail.com>
2025-10-17 18:50:21 -0700

    changes to LSTML

--- assignment3_NLP_fall25/models/naive/LSTM.py
+++ assignment3_NLP_fall25/models/naive/LSTM.py
@@ -56,24 +56,33 @@ class LSTM(nn.Module):
         #   You also need to include correct activation functions                      #
         ################################################################################
 
-        # i_t: input gate
-        self.weight_ii = nn.Parameter(torch.Tensor(input_size, hidden_size))
-        self.bias_ii = nn.Parameter(torch.Tensor(hidden_size))
-        self.weight_hi = nn.Parameter(torch.Tensor(hidden_size, hidden_size))
-        self.bias_hi = nn.Parameter(torch.Tensor(hidden_size))
-        # self.sigmoid_i = nn.Sigmoid()
+        # i_t: input gate  i_t = σ(x_t·W_ii + b_ii + h_{t-1}·W_hi + b_hi)
+        self.weight_i_t = nn.Parameter(torch.empty(input_size, hidden_size))  # W_ii
+        self.bias_i_t = nn.Parameter(torch.empty(hidden_size))  # b_ii
+        self.weight_h_t = nn.Parameter(torch.empty(hidden_size, hidden_size))  # W_hi
+        self.bias_h_t = nn.Parameter(torch.empty(hidden_size))  # b_hi
+        self.activation_i = nn.Sigmoid()
 
-        # f_t: the forget gate
-        self.weight_if =  nn.Parameter(torch.Tensor(input_size, hidden_size))
-        self.bias_if = nn.Parameter(torch.Tensor(hidden_size))
-        self.weight_hf = nn.Parameter(torch.Tensor(hidden_size, hidden_size))
-        self.bias_hf = nn.Parameter(torch.Tensor(hidden_size))
-        # self.sigmoid_f = nn.Sigmoid()
+        # f_t: forget gate  f_t = σ(x_t·W_if + b_if + h_{t-1}·W_hf + b_hf)
+        self.weight_f_t = nn.Parameter(torch.empty(input_size, hidden_size))  # W_if
+        self.bias_f_t = nn.Parameter(torch.empty(hidden_size))  # b_if
+        self.weight_h_f = nn.Parameter(torch.empty(hidden_size, hidden_size))  # W_hf
+        self.bias_h_f = nn.Parameter(torch.empty(hidden_size))  # b_hf
+        self.activation_f = nn.Sigmoid()
 
+        # g_t: cell candidate  g_t = tanh(x_t·W_ig + b_ig + h_{t-1}·W_hg + b_hg)
+        self.weight_g_t = nn.Parameter(torch.empty(input_size, hidden_size))  # W_ig
+        self.bias_g_t = nn.Parameter(torch.empty(hidden_size))  # b_ig
+        self.weight_h_g = nn.Parameter(torch.empty(hidden_size, hidden_size))  # W_hg
+        self.bias_h_g = nn.Parameter(torch.empty(hidden_size))  # b_hg
+        self.activation_g = nn.Tanh()
 
-        # g_t: the cell gate
-
-        # o_t: the output gate
+        # o_t: output gate  o_t = σ(x_t·W_io + b_io + h_{t-1}·W_ho + b_ho)
+        self.weight_o_t = nn.Parameter(torch.empty(input_size, hidden_size))  # W_io
+        self.bias_o_t = nn.Parameter(torch.empty(hidden_size))  # b_io
+        self.weight_h_o = nn.Parameter(torch.empty(hidden_size, hidden_size))  # W_ho
+        self.bias_h_o = nn.Parameter(torch.empty(hidden_size))  # b_ho
+        self.activation_o = nn.Sigmoid()
 
         ################################################################################
         #                              END OF YOUR CODE                                #
@@ -96,9 +105,39 @@ class LSTM(nn.Module):
         #   corresponding section of jupyter notebook. Iterate through all the time    #
         #   steps and return only the hidden and cell state, h_t and c_t.              #
         #   h_t and c_t should be initialized to zeros.                                #
-        #   Note that this time you are also iterating over all of the time steps.     #
+        #   ----------------------------------------------------------------------     #
+        #     it = σ(xt.Wii + bii + ht−1.Whi + bhi)                                    #
+        #     ft = σ(xt.Wif + bif + ht−1.Whf + bhf )                                   #
+        #     gt = tanh(xt.Wig + big + ht−1.Whg + bhg )                                #
+        #     ot = σ(xt.Wio + bio + ht−1.Who + bho)                                    #
+        #     ct = ft ⊙ ct−1 + it ⊙ gt                                                 #
+        #     ht = ot ⊙ tanh(ct)                                                       #
         ################################################################################
-        h_t, c_t = None, None  #remove this line when you start implementing your code
+        # h_t, c_t = None, None  #remove this line when you start implementing your code
+
+        batch, sequence, feature = x.shape
+        device = x.device
+        dtype = x.dtype
+
+        h_t = torch.zeros(batch, self.hidden_size, device=device, dtype=dtype)
+        c_t = torch.zeros(batch, self.hidden_size, device=device, dtype=dtype)
+
+        for t in range(sequence):
+            x_t = x[:, t, :]
+            i_t = self.activation_i(x_t @ self.weight_i_t + self.bias_i_t +
+                                    h_t @ self.weight_h_t + self.bias_h_t)
+
+            f_t = self.activation_f(x_t @ self.weight_f_t + self.bias_f_t +
+                                    h_t @ self.weight_h_f + self.bias_h_f)
+
+            g_t = self.activation_g(x_t @ self.weight_g_t + self.bias_g_t +
+                                    h_t @ self.weight_h_g + self.bias_h_g)
+
+            o_t = self.activation_o(x_t @ self.weight_o_t + self.bias_o_t +
+                                    h_t @ self.weight_h_o + self.bias_h_o)
+
+            c_t = f_t * c_t + i_t * g_t
+            h_t = o_t * torch.tanh(c_t)
         ################################################################################
         #                              END OF YOUR CODE                                #
         ################################################################################
